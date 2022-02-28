f8820eb09f728aa271c3fd4a8e2cdc19f1f82f32
Aaron Stokes <stokescomp01@gmail.com>
2022-02-28 12:29:03 -0700

    add empty array for data collection system

--- app.js
+++ app.js
@@ -1,14 +1,74 @@
 const inquirer = require('inquirer');
 
-inquirer
-    .prompt([
+
+const promptUser = () => {
+
+    return inquirer.prompt([
         {
             type: 'input',
             name: 'name',
             message: 'What is  your name?'
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your GitHub Username'
+    },
+    {
+        type: 'input',
+        name: 'about',
+        message: 'Provide some information about yourself:'
     }
-])
-.then(answers => console.log(answers));
+]);
+};
+
+const promptProject = portfolioData => {
+    //If there's no 'projects' array property, create one
+    if (!portfolioData.projects) {
+        portfolioData.projects = [];
+    } 
+    console.log(`
+    =================
+    Add a New Project
+    =================
+    `);
+        return inquirer.prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: 'What is the name of your project?'
+            },
+            {
+                type: 'input',
+                name: 'description',
+                message: 'Provide a description of the project (Required)'
+            },
+            {
+                type: 'checkbox',
+                name: 'languages',
+                message: 'What did you build this project with? (Check all that apply)',
+                choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node'] 
+            },
+            {
+                type: 'input',
+                name: 'link',
+                message: 'Enter the GitHub link to your project. (Required)'
+            },
+            {
+                type: 'confirm',
+                name: 'confirmAddProject',
+                message: 'Would you like to enter another project?',
+                default: false
+            }
+        ]);
+};
+
+
+promptUser()
+    .then(answers => console.log(answers))
+    .then(promptProject)
+    .then(projectAnswers => console.log(projectAnswers));
+
 
 
 
